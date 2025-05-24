416df1d606b0f6ad000776a3efcee45cabeb8eca
Juyel <juyelh@cdac.in>
2025-05-24 23:48:47 +0530

    Fix build to generate dist folder

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://cut2short-front.onrender.com",
+  base: 'https://cut2short-frontend.onrender.com',
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     sourcemap: false,
-    minify: "esbuild",
+    minify: 'esbuild',
   },
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname, "src/components"),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
     },
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
 });
